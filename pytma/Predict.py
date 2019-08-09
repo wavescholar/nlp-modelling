610450cb3e7c27b2d1b7a88180a48f69a69ea55a
Bruce Campbell <bruce@aloidia.solutions>
2019-08-09 19:08:52 -0400

    dev

---
+++ pytma/Predict.py
@@ -0,0 +1,18 @@
+
+
+
+##Text Classification Model using TF-IDF.
+#First, import the MultinomialNB module and create a Multinomial Naive Bayes classifier object using MultinomialNB() function.
+
+from sklearn.naive_bayes import MultinomialNB
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    text_counts, data['Sentiment'], test_size=0.3, random_state=1)
+
+# Model Generation Using Multinomial Naive Bayes
+clf = MultinomialNB().fit(X_train, y_train)
+predicted= clf.predict(X_test)
+print("MultinomialNB Accuracy:",metrics.accuracy_score(y_test, predicted))
