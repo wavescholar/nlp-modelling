ea322b116be314d15912e35788344a0970600a2c
Bruce Campbell <bruce@aloidia.solutions>
2019-08-17 14:57:45 -0400

    dev

--- pytma/Featurize.py
+++ pytma/Featurize.py
@@ -1,15 +1,23 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.tokenize import RegexpTokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
 
-class Featurize():
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import numpy as np
+
+from pytma.StopWord import StopWord
+from pytma.Tokenizer import Tokenizer
+
+
+class Featurize:
     """
     Class for featurizing text.  BOW, tf-idf, vector embeddings
     """
 
     def __init__(self, text):
         self.text = text
+        self.text_tokens = None
 
     # Term Frequency(TF) is the count the number of words occurred in each document.
     # The main issue with Term Frequency is that it will give more weight to longer documents.
@@ -25,7 +33,7 @@ class Featurize():
     def tf(self):
         # tokenizer to remove unwanted elements from out data like symbols and numbers
         token = RegexpTokenizer(r'[a-zA-Z0-9]+')
-        #stop_words : string {‘english’}, ‘english’is a built-in stop word list for English .
+        # stop_words : string {‘english’}, ‘english’is a built-in stop word list for English .
         # There are several known issues with ‘english’ and we should consider an alternative
         cv = CountVectorizer(lowercase=True, stop_words='english', ngram_range=(1, 1), tokenizer=token.tokenize)
         text_counts = cv.fit_transform(self.text.split())
@@ -36,25 +44,29 @@ class Featurize():
         import spacy
         nlp = spacy.load("en")
         text_tokens = nlp(self.text)
+        self.text_tokens = text_tokens
         text_vectors = np.vstack([word.vector for word in text_tokens if word.has_vector])
         return text_vectors
 
-    def pca_wv(self,vecs):
-        import matplotlib.pyplot as plt
-        from sklearn.decomposition import PCA
-        import numpy as np
+    def pca_wv(self, vecs):
+
+        if self.text_tokens == None:
+            self.wtv_spacy()
 
         pca = PCA(n_components=2)
         text_vecs_transformed = pca.fit_transform(vecs)
-        text_vecs_transformed = np.c_[self.text.split(), text_vecs_transformed]
+        text_vecs_transformed = np.c_[self.text_tokens, text_vecs_transformed]
 
-        x=text_vecs_transformed[:, 1]
-        y=text_vecs_transformed[:, 2]
+        x = text_vecs_transformed[:, 1]
+        y = text_vecs_transformed[:, 2]
         x = [float(i) for i in x]
-        y = [float(i) for i in y ]
+        y = [float(i) for i in y]
 
         plt.scatter(x, y)
         plt.xticks(x, rotation='vertical')
+        for i, txt in enumerate(self.text.split()):
+            plt.annotate(txt, (x[i], y[i]))
+
         plt.show()
 
 
@@ -67,7 +79,19 @@ if __name__ == '__main__':
     [2] He first achieved literary acclaim in his twenties with his semi-autobiographical trilogy,  \
     Childhood, Boyhood, and Youth (1852–1856), and Sevastopol Sketches (1855), based upon his experiences \
     in the Crimean War."
-    feat = Featurize(test_text)
+
+    text = Tokenizer.regexTokenize(test_text)
+
+    sw = StopWord(text)
+    swList = StopWord.SwLibEnum.spacy_sw
+
+    text = sw.remove(swList)
+
+    print(text)
+
+    text = " ".join(text)
+
+    feat = Featurize(text)
 
     text_tf = feat.tf()
 
@@ -80,3 +104,9 @@ if __name__ == '__main__':
     text_tf_idf = feat.tf_idf()
 
     print(text_tf_idf)
+
+    text_vectors = feat.wtv_spacy()
+
+    feat.pca_wv(text_vectors)
+
+    print("done")
